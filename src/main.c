99bc0c489b23290c5b8845fe9cb7e77d8074981c
jrhernn <jrhernn@proton.me>
2023-03-07 15:42:26 -0600

    first commit

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define TABLE_SIZE 100
+
+typedef struct
+{
+    int student_id;
+    char name[50];
+    char dob[25];
+    char course[50];
+} student;
+
+unsigned int hash()
+{
+}
+
+// Main function to test hash table functions
+int main()
+{
+}
